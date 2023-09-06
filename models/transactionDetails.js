3851b28eaa1799342f99524a49b9aac1542abaf4
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-06 13:50:44 +1000

    implement transactionDetail schema

---
+++ models/transactionDetails.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const transactionDetailSchema = new mongoose.Schema({
+    transactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+    },
+
+    startDate: {
+        type: Date,
+        required: true,
+    },
+
+    location: String,
+
+    endDate: Date,
+
+    rating1: Number,
+
+    rating2: Number,
+
+    review1: String,
+
+    review2: String,
+});
+
+const TransactionDetail = mongoose.model(
+    "TransactionDetail",
+    transactionDetailSchema
+);
+
+module.exports = TransactionDetail;
