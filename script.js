d23d1a8f786bb5011a01f40dfc6d93dbdda754d0
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-09 05:50:21 +1000

    add layout for transaction with collapsible

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-$(document).ready(function() {
-    $('#registerButton').click(function() {
-        window.location.href = '/register-page';
+$(document).ready(function () {
+    $("#registerButton").click(function () {
+        window.location.href = "/register-page";
     });
 });
 
-$(document).ready(function() {
-    setTimeout(function() {
-        $('.temp-message').fadeOut('slow', function() {
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".temp-message").fadeOut("slow", function () {
             $(this).remove();
         });
     }, 5000); // 5 seconds
 
-    $('.cancel-btn').on('click', function() {
-        const productId = $(this).data('product-id');
-        
+    $(".cancel-btn").on("click", function () {
+        const productId = $(this).data("product-id");
+
         $.ajax({
             url: `/api/products/${productId}`,
-            type: 'DELETE',
-            success: function(result) {
+            type: "DELETE",
+            success: function (result) {
                 // Display message
-                $('#message').text('The product has been cancelled').css({
-                    'background-color': 'green',
-                    'text-align': 'center',
-                    'padding': '10px',
-                    'color': 'white'
-                }).fadeIn().delay(5000).fadeOut(); 
+                $("#message")
+                    .text("The product has been cancelled")
+                    .css({
+                        "background-color": "green",
+                        "text-align": "center",
+                        padding: "10px",
+                        color: "white",
+                    })
+                    .fadeIn()
+                    .delay(5000)
+                    .fadeOut();
 
                 setTimeout(() => {
                     location.reload(); // refresh the page
                 }, 5100);
-            }
+            },
         });
     });
 });
 
-$(document).ready(function() {
-    $('#priceFilter').on('change', function() {
+$(document).ready(function () {
+    $("#priceFilter").on("change", function () {
         let range = $(this).val();
         window.location.href = "/item?priceRange=" + range;
+    });
+});
+
+$(document).ready(function () {
+    $("#view-message-btn").on("click", function () {
+        $(".transaction-chatbox").toggleClass("hidden");
     });
 });
