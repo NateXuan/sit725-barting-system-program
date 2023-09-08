79db3e23d2d144373e1d46c43784809b10ed49f6
Do Anh Khoa <s223188874@deakin.edu.au>
2023-09-09 07:06:49 +1000

    add createAt field

--- models/message.js
+++ models/message.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-    transactionId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "transaction",
+const messageSchema = new mongoose.Schema(
+    {
+        transactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "transaction",
+        },
+        messageType: {
+            type: String,
+            default: "text",
+            enum: ["text", "image"],
+        },
+        content: {
+            type: String,
+        },
+        url: {
+            type: String,
+        },
+        parentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "message",
+        },
+        isRead: {
+            type: Boolean,
+            default: False,
+        },
     },
-    messageType: {
-        type: String,
-        default: "text",
-        enum: ["text", "image"],
-    },
-    content: {
-        type: String,
-    },
-    url: {
-        type: String,
-    },
-    parentId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "message",
-    },
-    isRead: {
-        type: Boolean,
-        default: False,
-    },
-});
+    { timestamps: true }
+);
 
 const Message = mongoose.model("message", messageSchema);
 
